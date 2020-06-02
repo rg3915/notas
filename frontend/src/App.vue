b19eaa906e2a8bebe4ddf320bc600d28feae6186
rg3915 <rg3915@yahoo.com.br>
2020-06-02 00:00:51 -0300

    User app.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
+    <Navbar/>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Users/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
+import Users from './components/Users.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Navbar,
+    Users
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ frontend/src/components/Users.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <b-container>
+      <b-table striped hover :items="users"></b-table>
+    </b-container>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+
+const endpoint = 'http://localhost:8000/'
+
+export default {
+  data() {
+    return {
+      users: []
+    }
+  },
+  created() {
+    axios.get(endpoint + 'grade/')
+      .then(response => {
+        this.users = response.data.data;
+      })
+  }
+}
+</script>
